74c226717ab05060540351a0f3f2c1426afed717
Hayden <haydenc9898@gmail.com>
2025-02-15 16:46:07 -0500

    Update: Completed backend. Cleaned up codebase

--- backend/server.js
+++ backend/server.js
@@ -2,29 +2,16 @@ import express from 'express';
 import dotenv from "dotenv"
 import { connectDB } from './config/db.js'
 
+import productRoutes from "./routes/product.route.js";
+
 dotenv.config();
 
 const app = express();
 
-app.post("/products" , async (req, res) => {
-    const product = req.body; ///user sends this data
+app.use(express.json()); //allows app to accept JSON data into teh req.body
 
-    if(!product.name || !product.price || !product.image){
-        return res.status(400).json({ success:false, message: "Please Provide all fields"});
-    }
-
-    const newProduct = new Product(product)
-    
-    try{
-        await newProduct.save();
-        res.status(201).json({ success: true, data: newProduct});
-        //Code 201 indicates successful creation of object on server
-    } catch (error) {
-        console.error("Error in Create Product: " +  error.message)
-        res.status(500).json({ success: false, message: "Server Error"})
-        //Code 500 indicates internal server error
-    }
-});
+app.use("/api/products", productRoutes);
+//Mounts router with the prefix route
 
 app.listen(3000, () => {
     connectDB();
