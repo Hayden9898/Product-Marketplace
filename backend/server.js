ec864c3c503a3b8fbb9eaf4e1524ed46ed811cca
Hayden <haydenc9898@gmail.com>
2025-02-15 13:31:55 -0500

    fix: included API post method

--- backend/server.js
+++ backend/server.js
@@ -6,7 +6,25 @@ dotenv.config();
 
 const app = express();
 
-app.get("/products" , (req, res) => {});
+app.post("/products" , async (req, res) => {
+    const product = req.body; ///user sends this data
+
+    if(!product.name || !product.price || !product.image){
+        return res.status(400).json({ success:false, message: "Please Provide all fields"});
+    }
+
+    const newProduct = new Product(product)
+    
+    try{
+        await newProduct.save();
+        res.status(201).json({ success: true, data: newProduct});
+        //Code 201 indicates successful creation of object on server
+    } catch (error) {
+        console.error("Error in Create Product: " +  error.message)
+        res.status(500).json({ success: false, message: "Server Error"})
+        //Code 500 indicates internal server error
+    }
+});
 
 app.listen(3000, () => {
     connectDB();
