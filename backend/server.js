7d9cc6c1075fda67e46eb911b7ab931b313a2b57
Hayden <haydenc9898@gmail.com>
2025-02-20 02:40:00 -0500

    Update: Linked frontend to express backend. Added environment scripts

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import dotenv from "dotenv"
+import path from 'path';
 import { connectDB } from './config/db.js'
 
 import productRoutes from "./routes/product.route.js";
@@ -11,8 +12,19 @@ const PORT = process.env.PORT || 3000;
 
 app.use(express.json()); //allows app to accept JSON data into teh req.body
 
+const __dirname = path.resolve();
+
 app.use("/api/products", productRoutes);
 //Mounts router with the prefix route
+
+if(process.env.NODE_ENV === "production"){
+    app.use(express.static(path.join(__dirname, "/frontend/dist")));
+    //Returns user to index.html if they visit any other route
+    app.get("*", (req, res) => {
+        res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
+    });
+ 
+}
 
 app.listen(PORT, () => {
     connectDB();
