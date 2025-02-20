e2471b71007bb6b28c428ca69869aa5c4c43e9eb
Hayden <haydenc9898@gmail.com>
2025-02-20 02:12:50 -0500

    Update: Edit product button works, project functionality and frontend complete

--- frontend/src/store/product.js
+++ frontend/src/store/product.js
@@ -32,5 +32,21 @@ export const useProductStore = create((set) => ({
     //updates the UI immediately after deleting the product
     set((state) =>({ products: state.products.filter((product) => product._id !== pid)}));
     return {success: true, message: data.message};
-  }
+  },
+  updateProduct: async(pid, updatedProduct) => {
+    const res = await fetch(`/api/products/${pid}`, {
+      method: "PUT",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(updatedProduct),
+    });
+    const data = await res.json();
+    if(!data.success){ return {success: false, message: data.message};};
+    //updates the UI immediately without refreshing the page
+    set(state => ({
+      products: state.products.map(product => product._id === pid ? data.data : product)
+    }));
+    return {success: true, message: data.message};
+  },
 }));
